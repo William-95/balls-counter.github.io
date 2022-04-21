65b3ebb84284a573c09a7fd652ad5d52d8928d86
William-95 <101137872+William-95@users.noreply.github.com>
2022-04-21 19:25:51 +0200

    end

--- js/file.js
+++ js/file.js
@@ -2,42 +2,74 @@
 
 let field = document.querySelector('#field');
 let balls = document.querySelector('.balls');
-let form=document.querySelector("button");
-let increase = document.querySelector('#increase');
-let decrease = document.querySelector('#decrease');
-let reset = document.querySelector('#reset');
-let counter = document.querySelector('#counter');
+let form = document.querySelector("button");
 let count = 0;
 
+//create Counter
+
+let counter = document.createElement("span");
+counter.id = "counter";
+document.body.appendChild(counter);
+document.getElementById("counter").textContent = "0";
+field.after(counter);
+
+//create button
+let btns = document.createElement("div");
+btns.classList.add('counter');
+document.body.appendChild(btns);
+counter.after(btns);
+
+//btn increase
+let increase = document.createElement("button");
+increase.id = "increase";
+increase.classList.add('btn');
+document.querySelector('.counter').appendChild(increase);
+document.getElementById("increase").textContent = "+";
+
+//btn reset
+let reset = document.createElement("button");
+reset.id = "reset";
+reset.classList.add('btn');
+document.querySelector('.counter').appendChild(reset);
+document.getElementById("reset").textContent = "Reset";
+
+//btn decrease
+let decrease = document.createElement("button");
+decrease.id = "decrease";
+decrease.classList.add('btn');
+document.querySelector('.counter').appendChild(decrease);
+document.getElementById("decrease").textContent = "-";
+
+
 // button
-reset.addEventListener('mouseover',()=>{
+reset.addEventListener('mouseover', () => {
   let form = document.querySelector('#reset');
-form.style.color="red";
+  form.style.color = "red";
 });
 
-reset.addEventListener('mouseout',()=>{
-    let form=document.querySelector('#reset');
-    form.style.color="";
+reset.addEventListener('mouseout', () => {
+  let form = document.querySelector('#reset');
+  form.style.color = "";
 });
 
-increase .addEventListener('mouseover',()=>{
+increase.addEventListener('mouseover', () => {
   let form = document.querySelector('#increase');
-form.style.background="darkgreen";
+  form.style.background = "darkgreen";
 });
 
-increase .addEventListener('mouseout',()=>{
-    let form=document.querySelector('#increase');
-    form.style.background="";
+increase.addEventListener('mouseout', () => {
+  let form = document.querySelector('#increase');
+  form.style.background = "";
 });
 
-decrease.addEventListener('mouseover',()=>{
+decrease.addEventListener('mouseover', () => {
   let form = document.querySelector('#decrease');
-form.style.background="red";
+  form.style.background = "red";
 });
 
-decrease.addEventListener('mouseout',()=>{
-    let form=document.querySelector('#decrease');
-    form.style.background="";
+decrease.addEventListener('mouseout', () => {
+  let form = document.querySelector('#decrease');
+  form.style.background = "";
 });
 
 //counter+balls
